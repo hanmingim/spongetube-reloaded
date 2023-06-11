aba376f4a63dfccdea76bd63222be17a95bfa870
hanmingim <rlagksalsq3@gmail.com>
2023-06-11 17:23:43 +0900

    aws-upgrade

---
+++ example.ts
@@ -0,0 +1,9 @@
+
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+
+const region = "us-west-2";
+const client = new DynamoDB({ region });
+client.listTables({}, (err, data) => {
+    if (err) console.log(err, err.stack);
+    else console.log(data);
+});
