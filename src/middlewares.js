169b2e6cb502eb7f7daf43d77708561f62e81a32
hanmingim <rlagksalsq3@gmail.com>
2023-06-11 09:29:48 +0900

    Testing

--- src/middlewares.js
+++ src/middlewares.js
@@ -51,7 +51,7 @@ export const avatarUpload = multer({
   },
   storage: isHeroku ? s3ImageUploader : undefined,
 });
-export const videoUpload = multer({
+export const videoUpload = multer({//Testing
   dest: "uploads/videos", limits: {
     filesize: 10000000,
   },
